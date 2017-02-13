4f99bb448cbe6704aa3c024c98b7bad5702d5c0a
Matthew McCord <mattm.eternal@gmail.com>
2017-02-13 14:09:39 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ $( document ).ready(function() {
     $('.card').hover((event) => {
     	console.log(event.currentTarget);
 	    $(event.currentTarget).css('box-shadow', '0 15px 15px #333');
-	    $(event.currentTarget).css('border-radius', '20px');
     });
 
     $('.card').mouseleave((event) => {
     	let cards = $('.card');
     	cards.each((item) => {
     		$(cards[item]).css('box-shadow', '0 5px 15px #000');
-	 	 		$(event.currentTarget).css('border-radius', '10px');
     	});
     });
 });
