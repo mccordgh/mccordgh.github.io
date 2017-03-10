0b676e5194ad07d0a6d2f46bab334599798e62d8
Matthew McCord <mattm.eternal@gmail.com>
2017-03-10 15:52:40 -0600

    tap event

--- js/main.js
+++ js/main.js
@@ -97,12 +97,12 @@ $( document ).ready(function() {
 			chattyFlag = true;
 		}
 	});
-$('#whatever').on('touchstart click', function(){ /* do something... */ });
+
 	headShot.on('touchstart click', (event) => {
 		whatsTheDeal();	
 	});
 
-	dealGlassesEl.on('touchstart click', (event) => {
+	dealGlassesEl.on('tap click', (event) => {
 		whatsTheDeal();	
 	});
 
